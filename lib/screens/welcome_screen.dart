8e043b27dc692a7ad5b91d2269e0c7f5dcaaeb7b
Amaradipta <muhammad.amaradipta_ti22@nusaputra.ac.id>
2025-01-10 21:04:39 +0700

    welcome dah fix

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Get the dimensions of the screen
     final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
 
@@ -21,81 +20,86 @@ class WelcomeScreen extends StatelessWidget {
               ),
             ),
           ),
-          // Main content
           Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Stack(
-                  alignment: Alignment.bottomCenter,
-                  children: [
-                    Image.asset(
-                      'assets/images/maskot.png',
-                      width: 380,
-                      height: 380,
-                    ), // Mascot image
-                    Container(
-                      padding: EdgeInsets.only(bottom: 20), // Adjust padding as needed
+                Flexible(
+                  flex: 5,
+                  child: AspectRatio(
+                    aspectRatio: 1, // Untuk memastikan gambar tetap proporsional
+                    child: Stack(
+                      alignment: Alignment.bottomCenter,
+                      children: [
+                        Image.asset(
+                          'assets/images/maskot.png',
+                          fit: BoxFit.contain,
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(bottom: 20),
+                          child: Text(
+                            'Hello!',
+                            style: TextStyle(
+                              fontFamily: 'Joan',
+                              fontSize: screenWidth * 0.08,
+                              color: Color(0xFF132A13).withOpacity(0.7),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: screenHeight * 0.05),
+                Flexible(
+                  flex: 2,
+                  child: SizedBox(
+                    width: screenWidth * 0.9,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/login');
+                      },
                       child: Text(
-                        'Hello!',
+                        'Sign In',
                         style: TextStyle(
-                          fontFamily: 'Joan',
-                          fontSize: 32,
-                          color: Color(0xFF132A13).withOpacity(0.7),
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.bold,
+                          fontSize: screenWidth * 0.04,
+                          color: Color(0xFF98BD84),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 150),
-                // Sign In button with adaptive size
-                SizedBox(
-                  width: screenWidth * 0.9,
-                  height: screenHeight * 0.06,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/login');
-                    },
-                    child: Text(
-                      'Sign In',
-                      style: TextStyle(
-                        fontFamily: 'Roboto',
-                        fontWeight: FontWeight.bold,
-                        fontSize: 15,
-                        color: Color(0xFF98BD84), // Text color for Sign In
-                      ),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color(0xFF46932C),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFF46932C),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
                       ),
                     ),
                   ),
                 ),
-                SizedBox(height: 20),
-                // Create an Account button with adaptive size
-                SizedBox(
-                  width: screenWidth * 0.9,
-                  height: screenHeight * 0.06,
-                  child: OutlinedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/signup');
-                    },
-                    child: Text(
-                      'Create an Account',
-                      style: TextStyle(
-                        fontFamily: 'Roboto',
-                        fontWeight: FontWeight.bold,
-                        fontSize: 15,
-                        color: Color(0xFF46932C), // Text color for Create an Account
+                SizedBox(height: screenHeight * 0.02),
+                Flexible(
+                  flex: 2,
+                  child: SizedBox(
+                    width: screenWidth * 0.9,
+                    child: OutlinedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/signup');
+                      },
+                      child: Text(
+                        'Create an Account',
+                        style: TextStyle(
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.bold,
+                          fontSize: screenWidth * 0.04,
+                          color: Color(0xFF46932C),
+                        ),
                       ),
-                    ),
-                    style: OutlinedButton.styleFrom(
-                      backgroundColor: Color(0x2646932C), // Background color with 15% opacity
-                      side: BorderSide(color: Color(0xFF46932C), width: 2), // Border stroke
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
+                      style: OutlinedButton.styleFrom(
+                        side: BorderSide(color: Color(0xFF46932C), width: 2),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
                       ),
                     ),
                   ),
